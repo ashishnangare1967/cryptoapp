0db1c857b99ff2ddf2c8b01ee19abd47bd8c16dd
Ashish <ashishnangare1967@gmail.com>
2024-03-09 11:37:19 +0530

    crypto created

---
+++ src/components/Navigation.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav
+      className="w-full mt-16 flex gap-2 align-middle
+    border border-cyan rounded-lg p-2 
+    "
+    >
+      <NavLink
+        to="/"
+        end
+        className={({ isActive }) => {
+          return `w-full text-base flex items-center justify-center font-nunito
+
+${
+  isActive
+    ? "bg-cyan text-gray-300"
+    : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
+}
+    border-0 cursor-pointer rounded capitalize font-semibold`;
+        }}
+      >
+        Crypto
+      </NavLink>
+
+      <NavLink
+        to="/trending"
+        className={({ isActive }) => {
+          return `w-full text-base flex items-center justify-center font-nunito 
+
+${
+  isActive
+    ? "bg-cyan text-gray-300"
+    : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
+}
+    border-0 cursor-pointer rounded capitalize font-semibold`;
+        }}
+      >
+        trending
+      </NavLink>
+
+      <NavLink
+        to="/saved"
+        className={({ isActive }) => {
+          return `w-full text-base flex items-center justify-center font-nunito
+
+${
+  isActive
+    ? "bg-cyan text-gray-300"
+    : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
+}
+    border-0 cursor-pointer rounded capitalize font-semibold`;
+        }}
+      >
+        saved
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Navigation;
